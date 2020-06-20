05c554559b933ee3a1ae6a4f3059994a1b6f1a64
Taylor Grote <grotese@gmail.com>
2020-06-20 16:20:45 -0500

    Add Lerping Rotation to Player

--- Assets/Scripts/SpaceEaterInput.cs
+++ Assets/Scripts/SpaceEaterInput.cs
@@ -10,6 +10,7 @@ public class SpaceEaterInput : MonoBehaviour
     Vector3 _direction = Vector3.zero;
 
     [SerializeField] float _speed = 5f;
+    [SerializeField] float _rotationSpeed = 270f;   // degrees per second
 
     // When there's an input event, that means the input value has changed.
     // Continious dispatch doesn't happen, so we need to store the input value as state
@@ -27,6 +28,16 @@ public class SpaceEaterInput : MonoBehaviour
         var rigidBody = GetComponent<Rigidbody>();
         rigidBody.velocity = _direction * _speed;
 
+        // Only Modify Rotation is the direction isn't zero, because that means they just stopped input.
+        // We don't want to snap back to looking down the Z axis
+        if (_direction.sqrMagnitude > 0)
+        {
+            var targetRotation = Quaternion.LookRotation(rigidBody.velocity, Vector3.up);
+            var step = _rotationSpeed * Time.fixedDeltaTime;
+            rigidBody.MoveRotation(Quaternion.RotateTowards(rigidBody.rotation, targetRotation, step));
+        }
+
+        // Update Animation Parameters
         var animator = GetComponent<Animator>();
         var magnitude = rigidBody.velocity.magnitude;
         animator.SetFloat("Speed", rigidBody.velocity.magnitude);
