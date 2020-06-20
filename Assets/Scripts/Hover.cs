a8cb0143e94764a39ae80eb0c41ca2adf502f79d
Ross Waldron <zsigix@gmail.com>
2020-06-20 12:30:43 -0500

    Horrifying Changes
    * Add Monster staring down onto the world
    * Rotate the world with keyboard left/right arrows
    * Add floating script to Monster for more lively movement

---
+++ Assets/Scripts/Hover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hover : MonoBehaviour
+{
+    private Vector3 pos;
+    private float maxVariance = 0.25f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(pos.x, pos.y + (Mathf.Sin(Time.time) * maxVariance), pos.z);
+    }
+}
