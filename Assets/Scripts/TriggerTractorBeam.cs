62b2718c14598d487d6fb081cf97a8cd85b37193
Ross Waldron <zsigix@gmail.com>
2020-06-20 13:33:48 -0500

    Changes
    * Add Spacebar to Fire input
    * Change mood when Fire input is pressed

---
+++ Assets/Scripts/TriggerTractorBeam.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TriggerTractorBeam : MonoBehaviour
+{
+    [SerializeField] private AudioSource calmMusic;
+    [SerializeField] private AudioSource intenseMusic;
+    [SerializeField] private Light secondLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Fire(InputAction.CallbackContext context)
+    {
+        Debug.Log("GOT SOMETHING: ");
+        var newVal = context.ReadValue<float>();
+
+        calmMusic.volume = newVal == 1 ? 0 : 1;
+        intenseMusic.volume = newVal;
+        secondLight.intensity = newVal == 1 ? 2 : 0.75f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
