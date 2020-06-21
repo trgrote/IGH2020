24ffa9abc887ed6ea9eca5f94d809e8264658967
Taylor Grote <grotese@gmail.com>
2020-06-21 16:28:47 -0500

    Add Time remaining ui

---
+++ Assets/Scripts/WritePlanetRemaining.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class WritePlanetRemaining : MonoBehaviour
+{
+    [SerializeField] PlanetState _planetState;
+    Text _text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _text = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        _text.text = $"Pop. Left: {_planetState.RemainingPeople}\nTime Left: {(int)_planetState.RemainingTime}";
+    }
+}
