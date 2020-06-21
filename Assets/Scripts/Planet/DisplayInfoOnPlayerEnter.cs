d10e364d1445324969eb9a503fa99e78645c51d7
Taylor Grote <grotese@gmail.com>
2020-06-20 21:06:34 -0500

    Add Pop up info to all planets

---
+++ Assets/Scripts/Planet/DisplayInfoOnPlayerEnter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class DisplayInfoOnPlayerEnter : MonoBehaviour
+{
+    [SerializeField] TextMeshPro _text;
+    [SerializeField] LookAtConstraint _lookAtConstraint;
+
+    [SerializeField] rho.RuntimeGameObjectSet _players;
+
+    void OnTriggerEnter(Collider other)
+    {
+        // If the Player Enters, grab the main camera, then turn on the billboard
+        if (_players.Contains(other.gameObject))
+        {
+            _lookAtConstraint.SetSources(new List<ConstraintSource>(){
+                new ConstraintSource {
+                    sourceTransform = Camera.main.transform,
+                    weight = 1
+                }
+            });
+            _lookAtConstraint.enabled = true;
+
+            _text.enabled = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (_players.Contains(other.gameObject))
+        {
+            _text.enabled = false;
+
+            _lookAtConstraint.SetSources(new List<ConstraintSource>());
+            _lookAtConstraint.enabled = false;
+        }        
+    }
+}
