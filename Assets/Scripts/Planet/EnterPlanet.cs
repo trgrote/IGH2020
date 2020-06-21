b53b1d17acb1117dc5f2b955fc0fb2f48e623023
Taylor Grote <grotese@gmail.com>
2020-06-21 11:53:00 -0500

    Add Enter Planet

---
+++ Assets/Scripts/Planet/EnterPlanet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterPlanet : MonoBehaviour
+{
+    [SerializeField] so_events.Event _loadWorldSceneEvent;
+
+    public void Enter(PlanetInfo planetInfo, PlanetState planetState)
+    {
+        // Load PlanetInfo into planet state
+        planetInfo.LoadState(planetState);
+        _loadWorldSceneEvent.Raise();
+    }
+}
